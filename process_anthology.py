4ad680bca36c0d85cd6605d09649b4aac0ed4d81
Mo Arvan <mo.arvan@gmail.com>
2022-06-14 09:21:35 -0500

    Update process_anthology.py to handle github requests rate

--- process_anthology.py
+++ process_anthology.py
@@ -7,7 +7,8 @@ import os
 import time
 from builtins import enumerate
 from concurrent import futures
-
+from datetime import datetime
+from time import mktime
 import bibtexparser
 import bs4
 import requests
@@ -46,70 +47,56 @@ def cache_load_acl_anthology_bib(bib_path, export_dir):
     return acl_entries
 
 
-def try_get_webpage(url, headers, try_count=0):
-    response = None
-    request_failed = False
-    if try_count > 5:
-        return None
-    try:
-        response = requests.get(url=url, timeout=5, headers=headers)
-        if response.status_code == 404:
-            return None
-        if response.status_code != 200:
-            logging.error("status code {}".format(response.status_code))
-            request_failed = True
-    except Exception as e:
-        request_failed = True
-        logging.error("exception {}, {}".format(e, type(e)))
-    if request_failed:
-        logging.info("{} failed, fail count {}".format(url,
-                                                       try_count))
-        time.sleep(5)
-        try_get_webpage(url, headers, try_count + 1)
-
-    return response
-
-
-def parse_acl_anthologhy_webpage(response):
-    soup = BeautifulSoup(response.content, "lxml")
-
-    additional_information = []
-    link_div_block = soup.find_all("div", {"class": "acl-paper-link-block"})
-    table_div_block = soup.find_all("dl")
-
-    if len(link_div_block) != 0 and hasattr(link_div_block[0], "children"):
-        for c in link_div_block[0].children:
-            if isinstance(c, bs4.element.Tag):
-                key, url = c.text, c.attrs["href"]
-                additional_information.append((key, url))
-
-    if len(table_div_block) != 0:
-        children_list = list(table_div_block[0].children)
-        for i, c in enumerate(children_list):
-            if c.text in ["PDF:", "Software:", "Code", "Data"]:
-                key = c.text.replace(":", "")
-                url = " ".join([c.attrs["href"] for c in children_list[i + 1].children if hasattr(c, "href")])
-
-                if "," in url:
-                    logging.info("multi url {}".format(url))
-                additional_information.append((key, url))
-    return additional_information
-
-
-def parse_github_webpage(response: requests.Response):
-    response = response.json()
-    github_tuple_list = []
-    github_tuple_list.append(("stargazers_count", response["stargazers_count"]))
-    github_tuple_list.append(("forks_count", response["forks_count"]))
-    github_tuple_list.append(("open_issues_count", response["open_issues_count"]))
-    github_tuple_list.append(("updated_at", response["updated_at"]))
-    github_tuple_list.append(("created_at", response["created_at"]))
-    github_tuple_list.append(("pushed_at", response["pushed_at"]))
-
-    return github_tuple_list
+# def try_get_webpage(url, headers, try_count=0):
+#     response = None
+#     request_failed = False
+#     if try_count > 5:
+#         return None
+#     try:
+#         response = requests.get(url=url, timeout=5, headers=headers)
+#         if response.status_code == 404:
+#             return None
+#         if response.status_code != 200:
+#             logging.error("status code {}".format(response.status_code))
+#             request_failed = True
+#     except Exception as e:
+#         request_failed = True
+#         logging.error("exception {}, {}".format(e, type(e)))
+#     if request_failed:
+#         logging.info("{} failed, fail count {}".format(url,
+#                                                        try_count))
+#         time.sleep(5)
+#         try_get_webpage(url, headers, try_count + 1)
+#
+#     return response
 
 
 def get_reproducibility_information(entry: dict):
+    def parse_acl_anthology_webpage(response):
+        soup = BeautifulSoup(response.content, "lxml")
+
+        additional_information = []
+        link_div_block = soup.find_all("div", {"class": "acl-paper-link-block"})
+        table_div_block = soup.find_all("dl")
+
+        if len(link_div_block) != 0 and hasattr(link_div_block[0], "children"):
+            for c in link_div_block[0].children:
+                if isinstance(c, bs4.element.Tag):
+                    key, url = c.text, c.attrs["href"]
+                    additional_information.append((key, url))
+
+        if len(table_div_block) != 0:
+            children_list = list(table_div_block[0].children)
+            for i, c in enumerate(children_list):
+                if c.text in ["PDF:", "Software:", "Code", "Data"]:
+                    key = c.text.replace(":", "")
+                    url = " ".join([c.attrs["href"] for c in children_list[i + 1].children if hasattr(c, "href")])
+
+                    if "," in url:
+                        logging.info("multi url {}".format(url))
+                    additional_information.append((key, url))
+        return additional_information
+
     result = entry.copy()
 
     if "acl_status" in result and result["acl_status"] == "success":
@@ -122,12 +109,12 @@ def get_reproducibility_information(entry: dict):
         return result
     acl_url = entry["url"]
     try:
-        acl_page_response = requests.get(url=acl_url, timeout=5, headers=GLOBAL_HEADERS)
+        acl_page_response = requests.get(url=acl_url, timeout=2, headers=GLOBAL_HEADERS)
         if acl_page_response.status_code != 200:
             result["acl_status"] = "error {}".format(acl_page_response.status_code)
             return result
-
-        acl_info_tuple_list = parse_acl_anthologhy_webpage(acl_page_response)
+        # logging.info("{} success".format(acl_url))
+        acl_info_tuple_list = parse_acl_anthology_webpage(acl_page_response)
         for key, value in acl_info_tuple_list:
             if key in result:
                 if result[key] == value:
@@ -144,60 +131,94 @@ def get_reproducibility_information(entry: dict):
     return result
 
 
+def get_entry_github_url(entry):
+    code_url = next(filter(lambda x: "github.com/" in x, entry.values()), None)
+
+    if code_url is not None:
+        github_url = next(filter(lambda x: "github.com/" in x, code_url.split(" ")), None)
+        github_url_api = github_url.replace("github.com/", "api.github.com/repos/")
+        if len(github_url_api.split("/")) != 6:
+            github_url_api = "/".join(github_url_api.split("/")[:6])
+    else:
+        github_url_api = None
+    return github_url_api
+
+
 def has_reached_github_api_limit():
     rate_limit_url = "https://api.github.com/rate_limit"
-
     try:
-        response = requests.get(rate_limit_url)
+        global GLOBAL_HEADERS
+        headers = dict(GLOBAL_HEADERS)
+        github_token = "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"
+        headers['Authorization'] = 'token ' + github_token
+        response = requests.get(rate_limit_url, headers=headers)
         if response.status_code != 200:
             return True
-        result = response.json()
-        if result["rate"]["remaining"] == 0:
+        r = response.json()
+        if r["rate"]["remaining"] == 0:
             return True
         return False
     except Exception as e:
         return True
 
 
-def get_github_information(entry: dict):
-    result = entry.copy()
-
-    code_url = next(filter(lambda x: "github" in x, result.values()), None)
-
-    if "github_status" in result and result["github_status"] == "success":
-        # we have already processed the entry
+def get_github_api_rate_remaining_and_reset(github_auth_token):
+    rate_limit_url = "https://api.github.com/rate_limit"
+    remaining_requests, seconds_to_reset = 0, 5 * 60
+    try:
+        global GLOBAL_HEADERS
+        headers = dict(GLOBAL_HEADERS)
+        if github_auth_token != "":
+            headers['Authorization'] = 'token ' + github_auth_token
+        response = requests.get(rate_limit_url, headers=headers)
+        if not response.status_code != 200:
+            r = response.json()
+            seconds_to_reset = (
+                    datetime.fromtimestamp(mktime(time.gmtime(r["rate"]["reset"]))) - datetime.fromtimestamp(
+                mktime(time.gmtime()))).total_seconds()
+            remaining_requests = r["rate"]["remaining"]
+    except Exception as e:
         pass
-    elif has_reached_github_api_limit() or GITHUB_RATE_LIMIT_REACHED:
-        result["github_status"] = "reached max rate"
-    elif code_url is None:
-        # This method only works if we have a github url available
-        result["github_status"] = "missing"
-    else:
-        github_url = next(filter(lambda x: "github" in x, code_url.split(" ")), None)
+    return remaining_requests, seconds_to_reset
 
-        github_url_api = github_url.replace("github.com/", "api.github.com/repos/")
-        if len(github_url_api.split("/")) != 6:
-            github_url_api = "/".join(github_url_api.split("/")[:6])
-        try:
-            global GLOBAL_HEADERS
-            github_page_response = requests.get(url=github_url_api, timeout=30, headers=GLOBAL_HEADERS)
-            if github_page_response.status_code != 200:
-                result["github_status"] = "error {}".format(github_page_response.status_code)
-            else:
-                github_tuple_list = parse_github_webpage(github_page_response)
-                for key, value in github_tuple_list:
-                    if key in result:
-                        if result[key] == value:
-                            continue
-                        i = 1
-                        while key + "_{}".format(i) in result:
-                            i = i + 1
-                        key = key + "_{}".format(i)
-                    result[key] = value
-                result["github_status"] = "success"
 
-        except Exception as e:
-            result["github_status"] = "exception {}".format(type(e))
+def get_entry_github_information(entry: dict, github_url_api, github_auth_token):
+    def parse_github_webpage(response: requests.Response):
+        response = response.json()
+        github_tuple_list = []
+        github_tuple_list.append(("stargazers_count", response["stargazers_count"]))
+        github_tuple_list.append(("forks_count", response["forks_count"]))
+        github_tuple_list.append(("open_issues_count", response["open_issues_count"]))
+        github_tuple_list.append(("updated_at", response["updated_at"]))
+        github_tuple_list.append(("created_at", response["created_at"]))
+        github_tuple_list.append(("pushed_at", response["pushed_at"]))
+
+        return github_tuple_list
+
+    result = entry.copy()
+    try:
+        global GLOBAL_HEADERS
+        headers = dict(GLOBAL_HEADERS)
+        if github_auth_token != "":
+            headers['Authorization'] = 'token ' + github_auth_token
+        github_page_response = requests.get(url=github_url_api, timeout=2, headers=headers)
+        if github_page_response.status_code != 200:
+            result["github_status"] = "error {}".format(github_page_response.status_code)
+        else:
+            github_tuple_list = parse_github_webpage(github_page_response)
+            for key, value in github_tuple_list:
+                if key in result:
+                    if result[key] == value:
+                        continue
+                    i = 1
+                    while key + "_{}".format(i) in result:
+                        i = i + 1
+                    key = key + "_{}".format(i)
+                result[key] = value
+            result["github_status"] = "success"
+
+    except Exception as e:
+        result["github_status"] = "exception {}".format(type(e))
 
     return result
 
@@ -212,44 +233,76 @@ def export_acl(export_dir, acl_entries):
 
     csv_file_name = export_file_name + ".csv"
     with open(csv_file_name, 'w') as output_file:
-        dict_writer = csv.DictWriter(output_file, keys, quoting=csv.QUOTE_ALL, doublequote=True)
+        dict_writer = csv.DictWriter(output_file, keys,
+                                     quoting=csv.QUOTE_ALL,
+                                     doublequote=True)
         dict_writer.writeheader()
         dict_writer.writerows(acl_entries)
 
 
-def run_func_in_parrarell(func, inputs):
+def run_func_in_parallel(func, inputs):
     number_of_entries = len(inputs)
     with tqdm(total=number_of_entries) as progress_bar:
-        with concurrent.futures.ThreadPoolExecutor(max_workers) as executor:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
             future_list = {executor.submit(func, entry):
                                index for index, entry in enumerate(inputs)}
-            for future in concurrent.futures.as_completed(future_list, timeout=5):
+            for future in concurrent.futures.as_completed(future_list):
                 arg = future_list[future]
                 inputs[arg] = future.result()
                 progress_bar.update(1)
     return inputs
 
 
+def get_acl_information(acl_entries):
+    return run_func_in_parallel(get_reproducibility_information, acl_entries)
+
+
+def get_github_information(acl_entries, github_auth_token):
+    github_remaining_requests, github_time_to_reset = get_github_api_rate_remaining_and_reset(github_auth_token)
+    for i in tqdm(range(len(acl_entries))):
+        if github_remaining_requests == 0:
+            logging.info("Waiting {} seconds for github reset".format(github_time_to_reset))
+            time.sleep(github_time_to_reset + 5)
+            github_remaining_requests, github_time_to_reset = get_github_api_rate_remaining_and_reset(github_auth_token)
+
+        if "github_status" in acl_entries[i] and acl_entries[i]["github_status"] == "success":
+            # we have already processed the entry
+            continue
+        github_url_api = get_entry_github_url(acl_entries[i])
+        if github_url_api is None:
+            acl_entries[i]["github_status"] = "missing"
+            continue
+
+        # We need to avoid sending too many get requests to github's api
+        acl_entries[i] = get_entry_github_information(acl_entries[i], github_url_api, github_auth_token)
+        github_remaining_requests -= 1
+
+    return acl_entries
+
+
 def main():
-    parser = argparse.ArgumentParser(description='Downloading reproducibility data for ACL Anthology')
+    parser = argparse.ArgumentParser(
+        description='Downloading reproducibility data for ACL Anthology')
     parser.add_argument("--anthology_path", type=str)
     parser.add_argument("--export_dir", type=str)
+    parser.add_argument("--github_auth_token", type=str, default="")
 
     args = parser.parse_args()
     anthology_file_path = args.anthology_path
     export_dir = args.export_dir
+    github_auth_token = args.github_auth_token
 
     logging.info("Loading Bib")
     acl_entries = cache_load_acl_anthology_bib(anthology_file_path, export_dir)
 
     logging.info("Getting ACL Info")
-    acl_entries = run_func_in_parrarell(get_reproducibility_information, acl_entries)
+    acl_entries = get_acl_information(acl_entries)
 
     logging.info("Exporting intermediate results")
     export_acl(export_dir, acl_entries)
 
     logging.info("Getting GitHub Info")
-    acl_entries = [get_github_information(acl_entries[i]) for i in tqdm(range(len(acl_entries)))]
+    acl_entries = get_github_information(acl_entries, github_auth_token)
 
     logging.info("Exporting Results")
     export_acl(export_dir, acl_entries)
